67300f8f11ac952f8d0f61e243ead7313c368da8
Hope Bradley <hope.e.bradley@gmail.com>
2021-07-17 10:24:58 -0400

    add macros and validations to models

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,4 +1,10 @@
 class Job < ApplicationRecord
-    belongs_to :caregiver, class_name: "User"
-    belongs_to :employer, class_name: "User"
+    # belongs_to :caregiver, class_name: "User"
+    # belongs_to :employer, class_name: "User"
+
+    belongs_to :user
+
+    validates :title, presence: true, length: {minimum: 3}
+    validates :type, inclusion: {in: %w(children pets plants house other), message: "can't be blank"}
+    validates :salary, numericality: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ApplicationRecord
+    has_secure_password
     has_many :jobs
+
+    # QUESTION FOR NANCY: I want to use dependent destroy, but only if a "employer" deletes themselves. 
+    # Like if a caregiver deletes their account, the caregiver's jobs would just lose the caregiver_id but not be destroyed.
+
+    validates :username, presence: true, uniqueness: true, length: {minimum: 3}
+    validates :status, inclusion: {in: %w(caregiver employer), message: "can't be blank"}
+    validates :bio, length: {minimum: 10}
+    validates :password, length: {minimum: 4}
 end
