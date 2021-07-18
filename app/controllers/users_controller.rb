f7ee60012f5702d3f473d9b535a41dd9b584e1a6
Hope Bradley <hope.e.bradley@gmail.com>
2021-07-18 17:35:51 -0400

    add create action and helper methods to userscontroller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,32 @@
 class UsersController < ApplicationController
+
+    def create
+        user = User.create(user_params)
+        if user.valid?
+            session[:user_id] = user.id
+            render json: user, status: :created
+        else
+            render_unprocessable_entity_response(user)
+        end
+    end
+
+    private
+
+    def find_user
+        User.find_by(id: session[:user_id])
+    end
+
+    def user_params
+        params.permit(:username, :password, :password_confirmation, :name, :status, :bio, :img_url)
+    end
+
+    # exception handling helper methods
+    def render_unauthorized_response
+        render json: { errors: ["Unauthorized"]}, status: :unauthorized
+    end
+
+    def render_unprocessable_entity_response(user)
+        render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+    end
     
 end
