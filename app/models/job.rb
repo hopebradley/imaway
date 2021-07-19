226a7f234a025dc15c9084c40c88cbba46dc69c9
Hope Bradley <hope.e.bradley@gmail.com>
2021-07-19 11:48:11 -0400

    attempt caregiver/employer association without seperate models

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,10 +1,10 @@
 class Job < ApplicationRecord
-    # belongs_to :caregiver, class_name: "User"
-    # belongs_to :employer, class_name: "User"
 
-    belongs_to :user
+    belongs_to :caregiver, class_name: "User"
+    belongs_to :employer, class_name: "User"
 
-    validates :title, presence: true, length: {minimum: 3}
-    validates :category, inclusion: {in: %w(children pets plants house other), message: "can't be blank"}
-    validates :salary, numericality: true
+
+    # validates :title, presence: true, length: {minimum: 3}
+    # validates :category, inclusion: {in: %w(children pets plants house other), message: "can't be blank"}
+    # validates :salary, numericality: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,15 @@
 class User < ApplicationRecord
     has_secure_password
-    has_many :jobs
+    has_many :caregiver_jobs, foreign_key: "caregiver_id", class_name: "Job"
+    has_many :employer_jobs, foreign_key: "employer_id", class_name: "Job"
+
+
 
     # QUESTION FOR NANCY: I want to use dependent destroy, but only if a "employer" deletes themselves. 
     # Like if a caregiver deletes their account, the caregiver's jobs would just lose the caregiver_id but not be destroyed.
 
-    validates :username, presence: true, uniqueness: true, length: {minimum: 3}
-    validates :status, inclusion: {in: %w(caregiver employer), message: "can't be blank"}
-    validates :bio, length: {minimum: 10}
-    validates :password, length: {minimum: 4}
+    # validates :username, presence: true, uniqueness: true, length: {minimum: 3}
+    # validates :status, inclusion: {in: %w(caregiver employer), message: "can't be blank"}
+    # validates :bio, length: {minimum: 10}
+    # validates :password, length: {minimum: 4}
 end
