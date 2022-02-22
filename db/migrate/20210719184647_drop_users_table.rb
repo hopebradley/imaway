c3dde4df0605718f85074828161e46d64bbaa29c
Hope Bradley <hope.e.bradley@gmail.com>
2022-02-22 18:47:41 -0500

    remove users migrations

--- db/migrate/20210719184647_drop_users_table.rb
+++
@@ -1,5 +0,0 @@
-class DropUsersTable < ActiveRecord::Migration[6.1]
-  def change
-    drop_table :users
-  end
-end
--- db/migrate/20210723171422_delete_users_table.rb
+++
@@ -1,7 +0,0 @@
-class DeleteUsersTable < ActiveRecord::Migration[6.1]
-  def change
-    drop_table :users
-    drop_table :workouts
-  end
-
-end
